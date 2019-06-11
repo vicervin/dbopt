a456c12ad9473fc4f9c7a26a328bbc2426aa0607
vicervin <vicervin95@gmail.com>
2019-06-11 03:28:46 +0200

    csv_generator for old format of runs

---
+++ csv_generator.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import json
+
+import os
+from os.path import isfile, join
+
+def crunch_json(fn):
+    jsonRun = json.loads(open(fn).read())
+    dictRun = {}
+    dictRun.update({ f'conf_{k}': jsonRun['input'][k] for k in jsonRun['input']})
+    dictRun.update({ f'queryTime_{k}': jsonRun['output'][k] for k in jsonRun['output']})
+    fn = fn.split('/')[-1]
+    dictRun.update({ f'timestamp': fn.split('.')[0]})#fn.split('_')[0]})
+    dictRun.update({ f'benchmark': 'tpch'})#fn.split('_')[1]})
+    dictRun.update({ f'scale_factor': 10})#fn.split('_')[2]})
+    return dictRun
+
+
+#print(crunch_json('../results/1560200984_tpch_30.json'))
+def csv_generate(mydir):
+    #mydir = os.getcwd()+ '/'+ mydir
+    print(mydir)
+    onlyfiles = [f for f in os.listdir(mydir) if isfile(join(mydir, f)) and f.endswith('.json')]
+    df = pd.DataFrame()
+    for json in onlyfiles:
+        df = df.append(crunch_json(f'{mydir}/{json}'),ignore_index=True)
+    df.to_csv(f'{mydir}/complete.csv')
+    
+
+csv_generate('../tpch_10_50runs')
