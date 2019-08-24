1ec06563d290ccc8f51398a7c05d05593824b772
Ervin Madaha <vicervin95@gmail.com>
2019-08-24 03:29:26 +0200

    Converted scripts to classes

--- data_gen/data_generator.py
+++ data_gen/data_generator.py
@@ -7,11 +7,6 @@ import os
 
 FULL_PATH = os.path.dirname(os.path.abspath(__file__))
 
-USER = "postgres"
-#HOST = "localhost"
-HOST = "tpch_1"
-PORT = 5432
-DBNAME = "tpch"
 dbgen_workdir = FULL_PATH
 dbgen_executable = "dbgen"
 SCHEMA_PATH = f'{FULL_PATH}/schema.sql'
@@ -28,96 +23,105 @@ TPCH_TABLE_MAP = {
     'supplier': 's'
 }
 
-def generate_data(scale_factor):
-
-        chunks = math.ceil(scale_factor / 2)
-        print(f'Generating and ingesting Data for scale factor {scale_factor}')
-
-        # max_workers is not given, so it defaults to the number of processors on the machine
-        with ProcessPoolExecutor() as executor:
-            futures = {}
-
-            for table_name, table_code in TPCH_TABLE_MAP.items():
-                # These two are too small and dbgen seems to have a bug.
-                if table_name in ['region', 'nation']:
-                    future = executor.submit(ingest_table_chunk,
-                                             USER, HOST, PORT, DBNAME,
-                                             dbgen_workdir, dbgen_executable,
-                                             scale_factor,
-                                             table_name, table_code)
-                    futures[future] = f'{table_name}'
-                else:
-                    for build_steps in range(1, chunks + 1):
-                        future = executor.submit(ingest_table_chunk,
-                                                 USER, HOST, PORT, DBNAME,
-                                                 dbgen_workdir, dbgen_executable,
-                                                 scale_factor,
-                                                 table_name, table_code, chunks, build_steps)
-                        futures[future] = f'{table_name}_{build_steps}'
-
-            for future in as_completed(futures):
-                identifier = futures[future]
-                print(f'{identifier}. done: {future.done()}. result: {future.result()}')
-
-                if future.exception():
-                    raise AirflowException(f'DBGen failed in {identifier} '
-                                           f'with the exception {future.exception()}')
-        print(f"Data generated successfully for scale factor {scale_factor}")
-
-def ingest_table_chunk(user, host, port,
-                       dbname, dbgen_workdir, dbgen_executable,
-                       scale_factor,
-                       table_name, table_code, chunks=1, build_steps=1):
-    cmd = f'./{dbgen_executable} -fo -s {scale_factor} -T {table_code}'
-
-    if chunks > 1:
-        cmd += f' -C {chunks} -S {build_steps}'
-
-    sed_cmd = r'sed s/\|$//'
-
-    ingest_cmd = (f'psql -U {user} -h {host} -p {port} '
-                  f'-d {dbname} -w -c "COPY '
-                  f'{table_name} FROM STDIN WITH DELIMITER \'|\'"')
-
-    print(f'Running {cmd} | {sed_cmd} | {ingest_cmd}')
-    proc = Popen(f'{cmd} | {sed_cmd} | {ingest_cmd}',
-                 shell=True, cwd=dbgen_workdir,
-                 stdout=PIPE, stderr=PIPE)
-
-
-    output, error = proc.communicate()
-
-    if proc.returncode != 0:
-        raise Exception(f'DBGen failed in {table_name}_{build_steps} '
-                               f'with the exception {error}')
-
-    return output
-
-def run_psql_cmd(psql_cmd, db=True, file_=True):
-
-    plain_cmd = psql_cmd
-    if file_:
-        psql_cmd = '-f ' + psql_cmd
-    else :
-        psql_cmd = '-c ' + psql_cmd
+class DataGenerator:
     
-    if db:
-        psql_cmd = f"-d {DBNAME} {psql_cmd}"
+    def __init__(self, user='postgres', password='postgres', host='localhost', port=5432, dbname='tpch' ):
+        self.user = user
+        self.host = host
+        self.port = port
+        self.password = password
+        self.dbname = dbname
 
-    proc = Popen(f'psql -h {HOST} -U postgres {psql_cmd}',
-                    shell=True,
+    def generate_data(self, scale_factor):
+
+            chunks = math.ceil(scale_factor / 2)
+            print(f'Generating and ingesting Data for scale factor {scale_factor}')
+
+            # max_workers is not given, so it defaults to the number of processors on the machine
+            with ProcessPoolExecutor() as executor:
+                futures = {}
+
+                for table_name, table_code in TPCH_TABLE_MAP.items():
+                    # These two are too small and dbgen seems to have a bug.
+                    if table_name in ['region', 'nation']:
+                        future = executor.submit(ingest_table_chunk,
+                                                self.user, self.password, self.host, self.port, self.dbname,
+                                                dbgen_workdir, dbgen_executable,
+                                                scale_factor,
+                                                table_name, table_code)
+                        futures[future] = f'{table_name}'
+                    else:
+                        for build_steps in range(1, chunks + 1):
+                            future = executor.submit(ingest_table_chunk,
+                                                    self.user, self.password, self.host, self.port, self.dbname,
+                                                    dbgen_workdir, dbgen_executable,
+                                                    scale_factor,
+                                                    table_name, table_code, chunks, build_steps)
+                            futures[future] = f'{table_name}_{build_steps}'
+
+                for future in as_completed(futures):
+                    identifier = futures[future]
+                    print(f'{identifier}. done: {future.done()}. result: {future.result()}')
+
+                    if future.exception():
+                        raise AirflowException(f'DBGen failed in {identifier} '
+                                            f'with the exception {future.exception()}')
+            print(f"Data generated successfully for scale factor {scale_factor}")
+
+    def ingest_table_chunk(self, user, password, host, port,
+                        dbname, dbgen_workdir, dbgen_executable,
+                        scale_factor,
+                        table_name, table_code, chunks=1, build_steps=1):
+        cmd = f'./{dbgen_executable} -fo -s {scale_factor} -T {table_code}'
+
+        if chunks > 1:
+            cmd += f' -C {chunks} -S {build_steps}'
+
+        sed_cmd = r'sed s/\|$//'
+
+        ingest_cmd = (f'psql -U {user} --password {password} -h {host} -p {port} '
+                    f'-d {dbname} -c "COPY '
+                    f'{table_name} FROM STDIN WITH DELIMITER \'|\'"')
+
+        print(f'Running {cmd} | {sed_cmd} | {ingest_cmd}')
+        proc = Popen(f'{cmd} | {sed_cmd} | {ingest_cmd}',
+                    shell=True, cwd=dbgen_workdir,
                     stdout=PIPE, stderr=PIPE)
 
-    output, error = proc.communicate()
-    if proc.returncode != 0:
-            raise Exception(f'command "{plain_cmd}" failed, error : {error}')
 
-def run(scale_factor):
-    run_psql_cmd(f'"CREATE DATABASE {DBNAME};"', db=False, file_=False )
-    print('Generating Schema')
-    run_psql_cmd(SCHEMA_PATH)
-    generate_data(scale_factor)
-    print('Creating Indexes')
-    run_psql_cmd(INDEX_PATH)
-    print('Indexes Created')
+        output, error = proc.communicate()
+
+        if proc.returncode != 0:
+            raise Exception(f'DBGen failed in {table_name}_{build_steps} '
+                                f'with the exception {error}')
+
+        return output
+
+    def run_psql_cmd(self, psql_cmd, db=True, file_=True):
+
+        plain_cmd = psql_cmd
+        if file_:
+            psql_cmd = '-f ' + psql_cmd
+        else :
+            psql_cmd = '-c ' + psql_cmd
+        
+        if db:
+            psql_cmd = f"-d {self.dbname} {psql_cmd}"
+
+        proc = Popen(f'psql -h {self.host} -p {self.port} -U {self.user} --password {self.password} {psql_cmd}',
+                        shell=True,
+                        stdout=PIPE, stderr=PIPE)
+
+        output, error = proc.communicate()
+        if proc.returncode != 0:
+                raise Exception(f'command "{plain_cmd}" failed, error : {error}')
+
+    def run(scale_factor):
+        run_psql_cmd(f'"CREATE DATABASE {self.dbname};"', db=False, file_=False )
+        print('Generating Schema')
+        run_psql_cmd(SCHEMA_PATH)
+        generate_data(scale_factor)
+        print('Creating Indexes')
+        run_psql_cmd(INDEX_PATH)
+        print('Indexes Created')
 
--- runTpch.py
+++ runTpch.py
@@ -1,27 +1,16 @@
 import psycopg2
 import docker
+import traceback
 from subprocess import Popen, run
-from data_gen import data_generator
+from data_gen.data_generator import DataGenerator
 import json
 import time
 import os
 
-BENCHMARK = 'tpch'
-SCALE_FACTOR = 1
-CONTAINER_NAME = f'{BENCHMARK}_{str(SCALE_FACTOR)}'
-
 DBOPT_PATH = os.path.abspath(os.path.dirname(__file__))
-DATA_PATH = DBOPT_PATH+"/data_backups/"+CONTAINER_NAME
-
-#server = 'localhost'
-server = CONTAINER_NAME
-database = 'tpch'
-username = 'postgres'
-password = 'postgres'
 
 #QUERIES = [14,2,9,#20,6,#17,#18,8,#21,13,3,22,16,4,11,15,1,10,19,5,7,12]
 #QUERIES = [14,2,9,6,8,13,3,22,16,4,11,15,1,10,19,5,7,12]
-
 QUERIES = [14,2,9,"20_rewritten",6,"17_rewritten",8,13,3,22,16,4,11,15,1,10,19,5,7,12]
 confDict = {
     "max_parallel_workers" : "8" ,
@@ -38,198 +27,240 @@ confDict = {
     "random_page_cost" : "4" 
     }
 
-def run_tpch(confDict, scale_factor, results_dir=f'{DBOPT_PATH}/results'):
-    try:
-        #data_path = copy_data_dir(str_schema_scale)
-        stop_postgres()
-        pg_container = start_postgres(scale_factor)
-        set_config(confDict)
-        times = run_queries()
-        save_run(confDict, times, scale_factor, results_dir)
-        stop_postgres()
-        return times
-    except Exception as e:
-        print(e)
-        stop_postgres()
-        print("Tpch Run has failed and No Query Times returned")
-        return None
+class QueryRunner:
 
+    def __init__(self, user='postgres', host='localhost', port=5432, dbname='tpch', password='postgres',
+                        benchmark='tpch', scale_factor=1, dockerized = False, results_dir=f'{DBOPT_PATH}/results'):
+        self.host = host
+        if dockerized:
+            self.host = self.container_name
+        self.user = user
+        self.password = password
+        self.port = port
+        self.dbname = dbname
+        self.benchmark = benchmark
+        self.scale_factor = scale_factor
+        self.container_name = f'{benchmark}_{scale_factor}'
+        self.dockerized = dockerized
+        self.results_dir = results_dir
 
+        
 
-def stop_postgres():
-    docker_client = docker.from_env()  
-    running_pgs = docker_client.containers.list(all=True,filters={'ancestor':'postgres'})
-    #running_pgs_ = docker_client.containers.list(all=True,filters={'ancestor':f'{BENCHMARK}:{str(scale_factor)}'})
-    #running_pgs.extend(x for x in running_pgs_ if x not in running_pgs)
-    if running_pgs != []:
-        for pg_container in running_pgs:
-            pg_container.stop()
-            pg_container.remove()
-
-def wait_for_pg_to_start(db=database):
-    for i in range(1,100):
+    def run_tpch(self, confDict):
         try:
-            with psycopg2.connect(f'host={server} dbname={db} user={username} password={password}') as cnxn:
-                return
-        except psycopg2.Error as e:
-            time.sleep(6)
-    raise Exception("Database takes more than 10 minutes to start")
-
-def start_postgres(scale_factor):
-    try:
-        docker_client = docker.from_env()
-        current_pg = docker_client.containers.run(
-            image=f'{BENCHMARK}:{str(scale_factor)}',
-            detach=True,
-            environment={"PGDATA": "postgres/data"},
-            ports={5432: 5432},
-            name=CONTAINER_NAME,
-            links={'dbopt_py_1':'dbopt'},
-            network='dbopt_default',
-            #volumes={'postgresql.conf': {'bind': 'postgresql/data/postgresql.conf', 'mode': 'rw'}},
-            shm_size='3G')
-        wait_for_pg_to_start("postgres")
-        # TODO: Clear caches
-        return current_pg
+            self.stop_postgres()
+            pg_container = self.start_postgres()
+            self.set_config(confDict)
+            times = self.run_queries()
+            self.save_run(confDict, times, self.results_dir)
+            self.stop_postgres()
+            return times
+        except Exception as e:
+            print('runTpch failed due to an Exception:',e)
+            traceback.print_exc()
+            self.stop_postgres()
+            print("Tpch Run has failed and No Query Times returned")
+            return None
 
 
-    except docker.errors.APIError as e :
-        print(e)
 
-def check_config(confDict):
-    failed_config = []
-    for param, val in confDict.items():
-        result = pg_cmd(f"SHOW {param};")
-        if result == None:
-            print(f'!!! Config FAILURE : parameter "{param}" failed to set {val}, wrong parameter name or error in SQL command') 
-            failed_config.append(param)
-            continue
-        current_val = result[0][0]
-        if str(val) != current_val:
+    def stop_postgres(self):
+        docker_client = docker.from_env()  
+        running_pgs = docker_client.containers.list(all=True,filters={'ancestor':'postgres'})
+        #running_pgs_ = docker_client.containers.list(all=True,filters={'ancestor':f'{BENCHMARK}:{str(scale_factor)}'})
+        #running_pgs.extend(x for x in running_pgs_ if x not in running_pgs)
+        if running_pgs != []:
+            for pg_container in running_pgs:
+                pg_container.stop()
+                pg_container.remove()
 
-            print(f'!!! Config FAILURE : parameter "{param}" failed to set {val}, current value is {current_val}') 
-            failed_config.append(param)
-    [confDict.pop(param, None) for param in failed_config]
-    # if failed_config == [] :
-    #    print("Configurationss Successfully set")
+    def wait_for_pg_to_start(self, db):
+        for i in range(1,100):
+            try:
+                with psycopg2.connect(f'host={self.host} port={self.port} dbname={db} user={self.user} password={self.password}') as cnxn:
+                    return
+            except psycopg2.Error as e:
+                time.sleep(6)
+        raise Exception("Database takes more than 10 minutes to start")
 
-def check_data(scale_factor):
-    print(f'Checking Data Consistency for tpch:{scale_factor}')
-    rowsDict = { "nation": 25,
-    "customer":  150000,
-    "part":  200000,
-    "partsupp":  800000,
-    "orders":  1500000,
-    "region":  5,
-    "supplier":  10000,
-    "lineitem":  6001215}
-    for param, val in rowsDict.items():
-        if param not in ['region', 'nation']:
-            val = val * scale_factor
-        result = pg_cmd(f"SELECT count(*) FROM {param};")
-        if result == None:
-            print(f'!!! Data Mismatch : Expected table {param} is missing!!!') 
-            return False
-        current_val = result[0][0]
-        if (abs(current_val - val) / val) > 0.01 :
-            print(f'!!! Data Mismatch : Expected no of rows for table {param} is "{val}" current value is {current_val}') 
-            return False
-    print(f'Data is Consistent for tpch:{scale_factor}')    
-    return True
+    def start_postgres(self):
+        try:
+            docker_client = docker.from_env()
+            PGDATA = "postgres/data"
+            if self.dockerized:
+                current_pg = docker_client.containers.run(
+                    image=f'{self.benchmark}:{str(self.scale_factor)}',
+                    detach=True,
+                    environment={"PGDATA": PGDATA },
+                    ports={5432: self.port},
+                    name=self.container_name,
+                    links={'dbopt_py_1':'dbopt'},
+                    network='dbopt_default',
+                    #volumes={'postgresql.conf': {'bind': 'postgresql/data/postgresql.conf', 'mode': 'rw'}},
+                    shm_size='3G')
+            else:
+                current_pg = docker_client.containers.run(
+                    image=f'{self.benchmark}:{str(self.scale_factor)}',
+                    detach=True,
+                    environment={"PGDATA": PGDATA },
+                    ports={5432: self.port},
+                    name=self.container_name,
+                    shm_size='3G')
+            self.wait_for_pg_to_start("postgres")
+            # TODO: Clear caches
+            return current_pg
 
-def pg_cmd(cmd, autocommit = False, timeout=0):
-    try:
-        with psycopg2.connect(f'host={server} dbname={database} user={username} password={password}') as cnxn:
-            if autocommit:
-                cnxn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) 
-            with cnxn.cursor() as cursor:
-                #if timeout > 0 :
-                #    cursor.execute(f'SET STATEMENT_TIMEOUT={self.timeout * 1000}')
-                cursor.execute(cmd)
-                if cursor.description is not None:
-                    return cursor.fetchall()
-                return None
-    except psycopg2.Error as e:
-            print(f'Database connection error: {str(e)}')
+        except docker.errors.APIError as e :
+            print(e)
 
-def set_config(confDict):
-    for param in confDict:
-        pg_cmd(f"ALTER SYSTEM SET {param} = '{confDict[param]}';", autocommit=True)
-    pg_cmd(f"SELECT pg_reload_conf();")
-    check_config(confDict)            
-
-def write_config_file(fn, confDict):
-    with open(fn, 'w') as fp :
+    def check_config(self,confDict):
+        failed_config = []
         for param, val in confDict.items():
-            fp.writelines(f'{param} = {val}\n')
+            result = self.pg_cmd(f"SHOW {param};")
+            if result == None:
+                print(f'!!! Config FAILURE : parameter "{param}" failed to set {val}, wrong parameter name or error in SQL command') 
+                failed_config.append(param)
+                continue
+            current_val = result[0][0]
+            if str(val) != current_val:
 
-def run_queries():
-    times = {}
-    count = 0
-    for query in QUERIES:
-        start = time.time()
-        pg_cmd(open(f"{DBOPT_PATH}/queries/{query}.sql", "r").read())
-        times[query] = time.time() - start
-        #print(str(count+1) + " : Query "+ str(QUERIES[count]) + "| time : "+ str(time.time() - start) )
-        count += 1
-    times['Total'] = sum(times.values())
-    print("Total Query time : "+ str(times['Total']))
-    return times
+                print(f'!!! Config FAILURE : parameter "{param}" failed to set {val}, current value is {current_val}') 
+                failed_config.append(param)
+        [confDict.pop(param, None) for param in failed_config]
+        # if failed_config == [] :
+        #    print("Configurationss Successfully set")
 
-def save_run(confDict, times, scale_factor, results_dir):
-    dumpDict = {"workload":CONTAINER_NAME, "input": confDict, "output": times }
-    with open(f'{results_dir}/{round(time.time())}_{BENCHMARK}_{str(scale_factor)}.json', 'w') as fp:
-        json.dump(dumpDict, fp)
-
-def build_image(scale_factor):
-    image_name = f'{BENCHMARK}:{str(scale_factor)}'
-    stop_postgres()
-    try:
-        client = docker.from_env(timeout=600)
-        if client.images.list(name=image_name):
-            container = client.containers.run(
-                image=image_name,
-                detach=True,
-                environment={"PGDATA": "postgres/data"},
-                ports={5432: 5432},
-                links={'dbopt_py_1':'dbopt'},
-                network='dbopt_default',
-                name=CONTAINER_NAME)
-            wait_for_pg_to_start('postgres')
-            if check_data(scale_factor):
-                print(f"Machine already has a clean image {image_name}")
-                stop_postgres()
-                return True
-            else :
-                print(f"Deleting corrupted image {image_name}")
-                stop_postgres()
-                client.images.remove(image=image_name)
-                print("Building a new image")
-    
-        container = client.containers.run(
-                        image='postgres:latest',
-                        detach=True,
-                        ports={5432: 5432},
-                        network='dbopt_default',
-                        name=CONTAINER_NAME)
-        wait_for_pg_to_start('postgres')
-        data_generator.run(scale_factor)
-        if not check_data(scale_factor):
-            print(f"Building of image {image_name} failed due to inconsistent data")
-            return False
-        result = client.containers.get(CONTAINER_NAME).exec_run(cmd="mkdir /postgres")
-        if result.exit_code == 1 :
-            print(f"Error in Commiting container to image, {result.output}")
-        result = client.containers.get(CONTAINER_NAME).exec_run(cmd="cp -r /var/lib/postgresql/data/ /postgres/")
-        if result.exit_code == 1 :
-            print(f"Error in Commiting container to image, {result.output}")
-        container.commit(repository=BENCHMARK, tag=scale_factor)
-        stop_postgres()
-        print(f"Building of image {image_name} successfully complete")
+    def check_data(self):
+        print(f'Checking Data Consistency for tpch:{self.scale_factor}')
+        rowsDict = { "nation": 25,
+        "customer":  150000,
+        "part":  200000,
+        "partsupp":  800000,
+        "orders":  1500000,
+        "region":  5,
+        "supplier":  10000,
+        "lineitem":  6001215}
+        for param, val in rowsDict.items():
+            if param not in ['region', 'nation']:
+                val = val * self.scale_factor
+            result = self.pg_cmd(f"SELECT count(*) FROM {param};")
+            if result == None:
+                print(f'!!! Data Mismatch : Expected table {param} is missing!!!') 
+                return False
+            current_val = result[0][0]
+            if (abs(current_val - val) / val) > 0.01 :
+                print(f'!!! Data Mismatch : Expected no of rows for table {param} is "{val}" current value is {current_val}') 
+                return False
+        print(f'Data is Consistent for tpch:{self.scale_factor}')    
         return True
+
+    def pg_cmd(self, cmd, autocommit = False, timeout=0):
+        try:
+            with psycopg2.connect(f'host={self.host} port={self.port} dbname={self.dbname} user={self.user} password={self.password}') as cnxn:
+                if autocommit:
+                    cnxn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) 
+                with cnxn.cursor() as cursor:
+                    #if timeout > 0 :
+                    #    cursor.execute(f'SET STATEMENT_TIMEOUT={self.timeout * 1000}')
+                    cursor.execute(cmd)
+                    if cursor.description is not None:
+                        return cursor.fetchall()
+                    return None
+        except psycopg2.Error as e:
+                print(f'Database connection error: {str(e)}')
+
+    def set_config(self, confDict):
+        for param in confDict:
+            self.pg_cmd(f"ALTER SYSTEM SET {param} = '{confDict[param]}';", autocommit=True)
+        self.pg_cmd(f"SELECT pg_reload_conf();")
+        self.check_config(confDict)            
+
+    def write_config_file(self, fn, confDict):
+        with open(fn, 'w') as fp :
+            for param, val in confDict.items():
+                fp.writelines(f'{param} = {val}\n')
+
+    def run_queries(self):
+        times = {}
+        count = 0
+        for query in QUERIES:
+            start = time.time()
+            self.pg_cmd(open(f"{DBOPT_PATH}/queries/{query}.sql", "r").read())
+            times[query] = time.time() - start
+            #print(str(count+1) + " : Query "+ str(QUERIES[count]) + "| time : "+ str(time.time() - start) )
+            count += 1
+        times['Total'] = sum(times.values())
+        print("Total Query time : "+ str(times['Total']))
+        return times
+
+    def save_run(self, confDict, times, results_dir):
+        dumpDict = {"workload":self.container_name, "input": confDict, "output": times }
+        filename = f'{round(time.time())}_{self.benchmark}_{str(self.scale_factor)}'
+        with open(f'{DBOPT_PATH}/results/{results_dir}/{filename}.json', 'w') as fp:
+            json.dump(dumpDict, fp)
+
+    def build_image(self):
+        image_name = f'{self.benchmark}:{str(self.scale_factor)}'
+        PGDATA = "postgres/data"
+        self.stop_postgres()
+        try:
+            client = docker.from_env(timeout=600)
+            if client.images.list(name=image_name):
+                if self.dockerized:
+                    container = client.containers.run(
+                    image=image_name, detach=True,
+                    environment={"PGDATA": PGDATA }, ports={5432: self.port},
+                    links={'dbopt_py_1':'dbopt'},  network='dbopt_default',
+                    name=self.container_name)
+                else:
+                    container = client.containers.run(
+                    image=image_name,  detach=True,
+                    environment={"PGDATA": PGDATA}, ports={5432: self.port},
+                    name=self.container_name)
+                
+                self.wait_for_pg_to_start('postgres')
+                if self.check_data():
+                    print(f"Machine already has a clean image {image_name}")
+                    self.stop_postgres()
+                    return True
+                else :
+                    print(f"Deleting corrupted image {image_name}")
+                    self.stop_postgres()
+                    client.images.remove(image=image_name)
+                    print("Building a new image")
             
-    except docker.errors.APIError as e :
-        print(e)
+            if self.dockerized:
+                container = client.containers.run(
+                    image='postgres:latest', detach=True, ports={5432: self.port},
+                    network='dbopt_default',
+                    name=self.container_name)
+            else:
+                container = client.containers.run(
+                    image='postgres:latest', detach=True,
+                    ports={5432: self.port}, name=self.container_name)
+
+            self.wait_for_pg_to_start('postgres')
+            DataGenerator(
+                user=self.user, password=self.password, 
+                dbname=self.dbname, host=self.host, port=self.port
+                ).run(scale_factor)
+
+            if not self.check_data(scale_factor):
+                print(f"Building of image {image_name} failed due to inconsistent data")
+                return False
+            result = client.containers.get(self.container_name).exec_run(cmd="mkdir /postgres")
+            if result.exit_code == 1 :
+                print(f"Error in Commiting container to image, {result.output}")
+            result = client.containers.get(self.container_name).exec_run(cmd="cp -r /var/lib/postgresql/data/ /postgres/")
+            if result.exit_code == 1 :
+                print(f"Error in Commiting container to image, {result.output}")
+            container.commit(repository=self.benchmark, tag=scale_factor)
+            self.stop_postgres()
+            print(f"Building of image {image_name} successfully complete")
+            return True
+                
+        except docker.errors.APIError as e :
+            print(e)
 
 #run_tpch(confDict,1)
 #build_image(10)
